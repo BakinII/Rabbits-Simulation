d101c9fc3716edb1824a9139ba8d142e01953f34
BakinII <bakin_ii@mail.ru>
2023-04-21 20:50:57 +0700

    feat: добавить обьектам время жизни и уникальныецелочисленные идентификаторы
    * добавленны время жизни обьектам и целочисленные идентификаторы
    
    * реализованно хранение новых данных с помощью классов коллецкий
    
    * доработан интерфейс с добавлением новых параметров

--- src/main/java/org/example/model/RabbitList.java
+++ src/main/java/org/example/model/RabbitList.java
@@ -1,14 +1,20 @@
 package org.example.model;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class RabbitList {
     private static volatile RabbitList instance;
-    private List<Rabbit> rabbitList;
+    //private List<Rabbit> rabbitList;
+    private Vector<Rabbit> rabbitList;
+    private TreeSet<Integer> rabbitID;
+    //private TreeSet<Rabbit> rabbitTree;
+    private HashMap<Integer, Integer> rabbitBornTime;
 
-    private RabbitList(){
-        rabbitList = new ArrayList<>();
+    private RabbitList() {
+        rabbitList = new Vector<>();
+        rabbitID = new TreeSet<>();
+        rabbitBornTime = new HashMap<>();
+        //rabbitTree = new TreeSet<>();
     }
 
     public static RabbitList getInstance() {
@@ -19,14 +25,74 @@ public class RabbitList {
         return instance;
     }
 
-    public void addRabbit(Rabbit rabbit){
+    public void addRabbit(Rabbit rabbit) {
         rabbitList.add(rabbit);
+        rabbitID.add(rabbit.getId());
+        rabbitBornTime.put(rabbit.getId(), rabbit.getBornTime());
+        //rabbitTree.add(rabbit);
     }
 
-    public List<Rabbit> getRabbitList(){
+    public Vector<Rabbit> getRabbitList() {
         return rabbitList;
     }
-    public void clearRabbits(){
+
+    //public List<Rabbit> getRabbitList(){
+    //    return rabbitList;
+    //}
+    public boolean checkId(int id) {
+        return !rabbitID.contains(id);
+    }
+
+    public void clearRabbits() {
         rabbitList.clear();
+        rabbitID.clear();
+        rabbitBornTime.clear();
+    }
+    public void checkRabbitLifeTime(int rabbitAlbinoLifeTime,int rabbitClassicLifeTime, int time){
+        Vector<Integer> rabbitToDelete = new Vector<>();
+        for(Map.Entry<Integer,Integer> entry : rabbitBornTime.entrySet()){
+            Integer key = entry.getKey();
+            Integer value = entry.getValue();
+
+            boolean rabbitType =true;
+
+            for (Iterator<Rabbit> it = rabbitList.iterator();it.hasNext();){
+                Rabbit rabbit = it.next();
+                if (rabbit.getId() == key) {
+                    if(rabbit.getClass() == RabbitAlbino.class){
+                        rabbitType = false;
+                    }
+                    if(rabbit.getClass() == RabbitClassic.class){
+                        rabbitType = true;
+                    }
+                    break;
+                }
+            }
+
+            if(value + rabbitAlbinoLifeTime <= time && !rabbitType){
+                rabbitToDelete.add(key);
+            }
+
+            if(value + rabbitClassicLifeTime <= time && rabbitType){
+                rabbitToDelete.add(key);
+            }
+        }
+        rabbitToDelete.forEach(this::deleteRabbit);
+        rabbitToDelete.clear();
+    }
+    
+    private void deleteRabbit(int rabbitRemoveId){
+        rabbitBornTime.remove(rabbitRemoveId);
+        rabbitID.remove(rabbitRemoveId);
+        for (Iterator<Rabbit> it = rabbitList.iterator();it.hasNext();){
+            Rabbit rabbit = it.next();
+            if (rabbit.getId() == rabbitRemoveId) {
+                it.remove();
+                break;
+            }
+        }
+    }
+    public HashMap<Integer,Integer> getHashMapRabbit(){
+        return rabbitBornTime;
     }
 }
